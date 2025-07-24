546947af694e3d7a2c0d4ebf4f786f1a9bbeaca9
Abhilash73370 <abhilashpodishetty0705@gmail.com>
2025-07-24 23:51:21 +0530

    backend

--- Backend/config/middleware.js
+++ Backend/config/middleware.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 // CORS configuration
 const corsOptions = {
-  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175'],
+  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175', 'https://tempwall-1.onrender.com'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
